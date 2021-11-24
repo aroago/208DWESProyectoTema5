68f71b482b0e6f164f3f20fb1459151c66914689
aroa granero <aroa.graoma@educa.jcyl.es>
2021-11-24 08:40:31 +0100

    day 24/11/2021 create ProyectoTema5

---
+++ README.md
@@ -0,0 +1,24 @@
+# ProyectoTema 5
+
+**Módulo:** Desarrollo Web en Entorno Servidor
+
+# Autor: Aroa Granero Omañas
+
+**Fecha Inicio Proyecto: 24/11/2021**
+
+# Descripción
+
+- Ejercicios LogIn LogOff
+
+# Front end
+
+* HTML5
+* CSS3
+
+# Back end
+
+* PHP 7.4
+
+# Entornos
+
+- http://daw208.sauces.local/208DWESProyectoTema5/indexProyectoTema5.php
---
+++ indexProyectoTema5.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--Aroa Granero Omañas 
+Fecha Creacion: 24/11/2021
+Fecha Modificacion: 24/11/2021 -->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="aroaGraneroOma�as">
+        <meta name="application-name" content="Sitio web de DAW2">
+        <meta name="description" content="Inicio de la pagina web">
+        <meta name="keywords" content=" index" >
+        <meta name="robots" content="index, follow" />
+        <link href="./webroot/css/estilos.css"  rel="stylesheet"  type="text/css" title="Default style">
+        <link rel="shortcut icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AroaGO</title>
+    </head>
+    <body>
+        <header>
+            <h1>AROA G.O TEMA 5</h1>
+        </header>
+        <main>
+
+            <table>
+                <tr>
+                    <td>EJERCICIO</td>
+                    <td>PLAY</td>
+                    <td>CODIGO</td>
+                </tr>
+                <tr>
+                    <td>0. Mostrar el contenido de las variables superglobales y phpinfo().</td>
+                    <td class="hecho">
+                        <a href="./codigoPHP/ejercicio00.php"><img class="img" alt="Play" src="./webroot/img/play.png" /></a></td>
+                    <td>
+                        <a href="./mostrarcodigo/ejercicio00.php"> <img class="img" alt="Codigo" src="./webroot/img/codigo.png" /></a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>1. Desarrollo de un control de acceso con identificación del usuario basado en la función header()</td>
+                    <td class="hecho">
+                        <a href="./codigoPHP/ejercicio01.php"><img class="img" alt="Play" src="./webroot/img/play.png" /></a></td>
+                    <td>
+                        <a href="./mostrarcodigo/ejercicio01.php"> <img class="img" alt="Codigo" src="./webroot/img/codigo.png" /></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2. Desarrollo de un control de acceso con identificación del usuario basado en la función header() y
+						en el uso de una tabla “Usuario” de la base de datos. (PDO).</td>
+                    <td class="hecho">
+                        <a href="./codigoPHP/ejercicio02.php"><img class="img" alt="Play" src="./webroot/img/play.png" /></a></td>
+                    <td>
+                        <a href="./mostrarcodigo/ejercicio02.php"> <img class="img" alt="Codigo" src="./webroot/img/codigo.png" /></a>
+                    </td>
+                </tr>
+               
+            </table>
+
+        </main>
+        <footer id="footerP">
+            <p>&copy;2021 Todos los derechos reservados AroaGO</p>
+            <div id="iconos">
+                <a type="application/github" href="https://github.com/aroago" target="_blank">
+                    <img class="iconoIMG" alt="gitHub" src="./webroot/img/github.png" />
+                </a>
+            </div>
+        </footer>
+    </body>
+</html>
