5d703d3d001207832010aad1893594132897be95
aroa granero <aroa.graoma@educa.jcyl.es>
2021-11-24 09:25:12 +0100

    day 24/11/2021 modify ej00

--- codigoPHP/ejercicio00.php
+++ codigoPHP/ejercicio00.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <!--Aroa Granero Omañas 
-Fecha Creacion: 28/09/2021
-Fecha Modificacion: 29/09/2021 -->
+Fecha Creacion: 24/11/2021
+Fecha Modificacion: 24/11/2021 -->
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="author" content="aroaGraneroOmañas">
-        <title>Ejercicio01</title>
+        <title>Ejercicio 00</title>
     </head>
     <body>
         <?php
-        //con "echo" visualizo el texto en pantalla.
-            echo "hola Mundo";
         //phpinfo() utilizada para saber todala informacion y todo lo que contiene mi php.
             phpinfo();
+            
         ?>
     </body>
 </html>
